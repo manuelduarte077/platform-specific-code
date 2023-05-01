77c8c7e2f6c5332a71d84b04bef0a82c15c133ca
Manuel Duarte  <carlosd99m@gmail.com>
2023-04-30 22:05:47 -0600

    get cell phone ringtones

--- native_ringtone/lib/main.dart
+++ native_ringtone/lib/main.dart
@@ -1,18 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_sound_lite/flutter_sound.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  List ringtones = [];
+  var player = FlutterSoundPlayer();
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Native Ringtone'),
+          elevation: 0,
+        ),
+        body: Column(
+          children: [
+            if (ringtones.isNotEmpty)
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: ringtones.length,
+                  itemBuilder: (context, index) {
+                    final ringtone = ringtones[index];
+                    return Card(
+                      elevation: 0,
+                      color: Colors.deepPurple[50],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: ListTile(
+                        onTap: () async {
+                          player = (await player.openAudioSession())!;
+                          player.startPlayer(
+                            fromURI:
+                                '/system/media/audio/ringtones/$ringtone.ogg',
+                            codec: Codec.opusOGG,
+                            whenFinished: () {
+                              player.stopPlayer();
+                            },
+                            fromDataBuffer: null,
+                          );
+                        },
+                        title: Text(ringtone),
+                        subtitle: ringtone.contains('notification')
+                            ? const Text('Notification')
+                            : const Text('Ringtone'),
+                        trailing: const Icon(Icons.play_arrow),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            Center(
+              child: FilledButton(
+                onPressed: () async {
+                  const channel = MethodChannel('native_ringtone');
+                  ringtones = await channel.invokeMethod('getRingtones');
+
+                  setState(() {
+                    ringtones = ringtones;
+                  });
+                },
+                child: const Text('Get Ringtones'),
+              ),
+            ),
+          ],
         ),
       ),
     );
