cb5752b82a5a70c6429bb0c431edc63dd3283767
Manuel Duarte  <carlosd99m@gmail.com>
2023-05-01 16:47:15 -0600

    feat: notify that the network is disconnected

---
+++ platform_channel_events/ios/Runner/Constants.swift
@@ -0,0 +1,8 @@
+
+struct Constants {
+    static let wifi = 0xFF
+    static let cellular = 0xEE
+    static let disconnected = 0xDD
+    static let unknown = 0xCC
+    static let eof = -0xFFFFFF
+}
