99f6fff8c3c1a6a8596d267e64e0ad5202e78295
Manuel Duarte  <carlosd99m@gmail.com>
2023-06-12 22:33:52 -0600

    feat: background location tracking

---
+++ BackgroundLocationTracking/app/src/main/java/io/manuelduarte077/backgroundlocationtracking/LocationClient.kt
@@ -0,0 +1,10 @@
+package io.manuelduarte077.backgroundlocationtracking
+
+import android.location.Location
+import kotlinx.coroutines.flow.Flow
+
+interface LocationClient {
+    fun getLocationUpdates(interval: Long): Flow<Location>
+
+    class LocationException(message: String) : Exception()
+}
